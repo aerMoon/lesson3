049db9433fe88b830d9bbb605343502f500b63ab
aerMoon <silfochka@gmail.com>
2023-02-06 16:04:10 +0500

    !!

--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Program {
+import java.util.ArrayList;
+import java.util.*;
+
+class Program {
+
+    public static void main(String[] args){
+        //создание списка задач (пустой)
+        ArrayList<UserTask> taskList = new ArrayList<UserTask>();
+
+        UserTask task1 = new UserTask("заголовок", "текст заметки/задачи");
+
+        taskList.add(task1); //добавление одного элемента
+        Scanner in = new Scanner(System.in);
+
+        //в цикле, пока не введут пустую строку добавляются
+        //новые задачи в список
+        do {
+            System.out.println("Введите заголовок заметки>>");
+            String title = in.nextLine();
+            System.out.println("Введите текст заметки>>");
+            String text = in.nextLine();
+            taskList.add(new UserTask(title, text));
+        }
+        while (in.nextLine()!="");
+        in.close();
+
+        for(UserTask task : taskList){
+            task.print();
+
+        }
+        System.out.println("после удаления 1 заметки>>");
+        taskList.remove(1);
+        for(UserTask task : taskList){
+            task.print();
+
+        }
+    }
 }
--- src/UserTask.java
+++ src/UserTask.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class UserTask {
+import java.util.Date;
+
+public class UserTask {
+    String title;
+    String text;
+    Date date;
+
+
+    UserTask(String title, String text){
+        this.date = new Date();
+        this.text = text;
+        this.title = title;
+    }
+
+    void print(){
+        System.out.println(title+ "\n" +text +"\n"+date);
+    }
 }
